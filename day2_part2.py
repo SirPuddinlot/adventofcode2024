15d94d1c44b0b6298e5480fe58883f685b2b33ea
Atzhang <41776644+SirPuddinlot@users.noreply.github.com>
2024-12-04 23:35:00 -0800

    Update day2_part2.py

--- day2_part2.py
+++ day2_part2.py
@@ -42,3 +42,41 @@ with open("test.txt") as f:
     for line in f:
         process2(line)
 print("ans ", total)
+
+#garbage below
+
+total = 0
+def process2(line):
+    count = 0
+    line2 = line.split()
+    line2 = [int(i) for i in line2]
+    def process(line, count):
+        global total
+        flag = line[0]-line[1]
+        for i in range(len(line) - 1):
+            diff = line[i]-line[i+1]
+            if count < 2:
+                if (abs(diff) > 3) or (flag > 0 and diff < 0) or \
+                (flag < 0 and diff > 0) or (diff == 0):
+                    print("removing ", line[i], " index " , i, " in ", line)
+                    line.pop(i)
+                    count += 1
+                    process(line, count) 
+                    return  
+            else:
+                print("Failed ", line)
+                return
+        print("Success ", line)
+        with open("output.txt", "w") as f:
+            f.write(" ".join(map(str, line)))
+        total += 1
+    process(line2, count)
+with open("test.txt") as f:
+    # for line in f:
+    #     process2(line)
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        process2(line)
+print("ans ", total)
